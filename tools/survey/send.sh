dd7b8e25f7d4526f8a19be3c1c7dced79cc54f9f
cybuletz <118841368+cybuletz@users.noreply.github.com>
2026-06-10 15:07:06 +0300

    Delete tools/survey/send.sh

--- tools/survey/send.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Usage: bash tools/survey/run_survey.sh "Survey Title" "email1,email2" "Friday EOD"
-python3 tools/survey/survey.py \
-  --title "$1" \
-  --emails "$2" \
-  --questions tools/survey/questions.json \
-  --deadline "$3"
